d7838fb9f132e00c52f2cebe40614ade48e6beb1
Tim Colson <timcolson@42clients.com>
2021-02-04 16:00:24 -0700

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Tim Colson on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
